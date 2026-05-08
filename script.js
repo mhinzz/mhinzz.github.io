d539788d8d5edfe148cbf3d6d44a9e4ac1bbeed0
Matthew Hinz <43189125+mhinzz@users.noreply.github.com>
2026-05-08 02:21:45 -0400

    More Tweeks

--- script.js
+++ script.js
@@ -65,7 +65,23 @@ function initialiseSidebarOffset() {
 }
 
 
+// Highlight Current Page in Navigation
+function initialiseActiveNavLink() {
+	const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+	const navLinks = document.querySelectorAll('nav a');
+	
+	navLinks.forEach(link => {
+		const href = link.getAttribute('href');
+		if (href === currentPage) {
+			link.classList.add('active');
+		} else {
+			link.classList.remove('active');
+		}
+	});
+}
+
 // Run
 initialiseSectionAnimations();
 initialiseThemeToggle();
 initialiseSidebarOffset();
+initialiseActiveNavLink();
